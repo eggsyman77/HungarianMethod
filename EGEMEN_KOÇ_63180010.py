572138f359a9c798e4e3a9eac5f73e77f3635c8f
eggsyman77 <97639704+eggsyman77@users.noreply.github.com>
2022-01-15 14:01:21 +0300

    Add files via upload

--- EGEMEN_KOÇ_63180010.py
+++ EGEMEN_KOÇ_63180010.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 def row_minimization(zeromatrix, zeromark):
+#comment: the row can be found
     rowofminimum =[63180010,0]
     for number_of_row in range(zeromatrix.shape[0]):
         if np.sum(zeromatrix[number_of_row] == True) > 0 and rowofminimum[0] > np.sum(zeromatrix[number_of_row] == True):
             rowofminimum = [np.sum(zeromatrix[number_of_row] == True),number_of_row]
+#comment: According to false case, row and column(special) is marked
     zeroindex = np.where(zeromatrix[rowofminimum[1]] == True)[0][0]
     zeromark.append((rowofminimum[1], zeroindex))
     zeromatrix[rowofminimum [1], :] = False
@@ -12,6 +14,7 @@ def row_minimization(zeromatrix, zeromark):
 
 def matrixmark (matrix):
     currentmatrix = matrix
+#comment: for zeroboolean matrix (True==0, ow==False)
     zeroboolean_matrix = (currentmatrix == 0)
     zeroboolean_matrix_copy = zeroboolean_matrix.copy()
     zeromarked = []
@@ -43,6 +46,7 @@ def matrixmark (matrix):
 def matrixadjust(matrix, rowscover,columnscover ):
     currentmatrix = matrix
     zeroelement_none = []
+#comment: Determine the minimumnumber
     for row in range(len(currentmatrix)):
         if row not in rowscover:
             for s in range(len(currentmatrix[row])):
@@ -62,49 +66,48 @@ def matrixadjust(matrix, rowscover,columnscover ):
 def hungarian_method(matrix):
     dimension = matrix.shape[0]
     currentmatrix = matrix
+#comment:the first step would be subtracting operation (for every row and every column)
     for numberofrow in range(matrix.shape[0]):
         currentmatrix[numberofrow] = currentmatrix[numberofrow] - np.min(currentmatrix[numberofrow])
     for numberofcolumn in range(matrix.shape[1]):
         currentmatrix[:, numberofcolumn] = currentmatrix[:, numberofcolumn] - np.min(currentmatrix[:, numberofcolumn])
     zero_num = 0
     while zero_num < dimension:
-        solution_mitsubishi, rowmarked, columnmarked= matrixmark(currentmatrix)
+        solution_kumtel, rowmarked, columnmarked= matrixmark(currentmatrix)
         zero_num = len(rowmarked) + len(columnmarked)
         if zero_num < dimension:
             currentmatrix = matrixadjust(currentmatrix, rowmarked, columnmarked)
-    return solution_mitsubishi
+    return solution_kumtel
 
-def calculation(matrix,mitsubishi):
+def calculation(matrix,kumtel):
     total = 0
     solutionofmatrix = np.zeros((matrix.shape[0], matrix.shape[1]))
-    for s in range(len(mitsubishi)):
-        total += matrix[mitsubishi[s][0], mitsubishi[s][1]]
-        matrix[mitsubishi[s][0], mitsubishi[s][1]] = matrix[mitsubishi[s][0], mitsubishi[s][1]]
+    for s in range(len(kumtel)):
+        total += matrix[kumtel[s][0], kumtel[s][1]]
+        matrix[kumtel[s][0], kumtel[s][1]] = matrix[kumtel[s][0], kumtel[s][1]]
     return total,solutionofmatrix
 
-def main():
-    mitsubishi_productioncost_matrix = np.array([[61,33 , 18, 6, 13],
-                                                [36, 81, 66, 10, 30],
-                                                [8, 60, 80, 1, 3],
-                                                [11, 18, 31, 6, 8],
-                                                [6, 8, 16, 10, 81]])
-    solution_mitsubishi = hungarian_method(mitsubishi_productioncost_matrix.copy())
-    solution, solutionofmatrix = calculation(mitsubishi_productioncost_matrix, solution_mitsubishi)
+def KumtelExample():
+    kumtel_productioncost_matrix = np.array([[16, 10, 66, 30, 86],
+                                            [8, 60, 80, 13, 31],
+                                            [6, 81, 36, 8, 10],
+                                            [33, 30, 68, 13, 80],
+                                            [10, 36, 83, 60, 66]])
+    solution_kumtel = hungarian_method(kumtel_productioncost_matrix.copy())
+    solution, solutionofmatrix = calculation(kumtel_productioncost_matrix,solution_kumtel)
 
-    mitsubishi_profitmatrix = np.array([[2, 3, 12, 2, 3],
-                                       [2, 5, 2, 3, 1],
-                                       [2, 1, 1, 2, 3],
-                                       [7, 12, 2, 4, 2],
-                                       [2, 2, 0, 1, 2]])
-    maximumvalue = np.max(mitsubishi_profitmatrix)
-    mitsubishi_productioncost_matrix = maximumvalue - (mitsubishi_profitmatrix)
-    solution_mitsubishi = hungarian_method(mitsubishi_productioncost_matrix.copy())
-    solution, solutionofmatrix = calculation(mitsubishi_profitmatrix, solution_mitsubishi)
+    kumtel_profitmatrix = np.array([[2, 1, 2, 3, 2],
+                                   [2, 3, 1, 3, 5],
+                                   [4, 7, 0, 3, 8],
+                                   [3, 3, 12, 3, 1],
+                                   [6, 2, 3, 3, 2]])
+    maximumvalue = np.max(kumtel_profitmatrix)
+    kumtel_productioncost_matrix = maximumvalue - (kumtel_profitmatrix)
+    solution_kumtel = hungarian_method(kumtel_productioncost_matrix.copy())
+    solution, solutionofmatrix = calculation(kumtel_profitmatrix, solution_kumtel)
+    print(maximumvalue-kumtel_profitmatrix)
+    print(kumtel_profitmatrix)
+    print(f"Kumtel Infrared Heaters assignment problem solution is: {solution:}")
 
-    print(maximumvalue-mitsubishi_profitmatrix)
-    print(mitsubishi_profitmatrix)
 
-    print(f"Mitsubishi Air Conditioner assignment problem solution is: {solution:}")
-
-if __name__ == '__main__':
-    main()
+KumtelExample()
